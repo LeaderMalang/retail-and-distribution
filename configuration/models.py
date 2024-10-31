d79b6b23896a0b32f7e1f9fccf635520ce8b1359
razis928 <razis928@gmail.com>
2024-11-01 00:53:11 +0500

    General Ledger accounts added

--- configuration/models.py
+++ configuration/models.py
@@ -4,6 +4,10 @@ from django.core.exceptions import ValidationError
 
 class BookingMan(models.Model):
     name = models.CharField(max_length=255)
+    total_sales = models.CharField(max_length=255, null=True)
+    sales_commission = models.CharField(max_length=255, null=True)
+    sales_target = models.CharField(max_length=255, null=True)
+    targeted_month = models.CharField(max_length=255, null=True)
 
     def __str__(self):
         return self.name
